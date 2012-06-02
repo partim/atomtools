3b9c3cdb0b00a2b6dafca8528c0c56ee041693d3
Martin Hoffmann <hn@nvnc.de>
2012-06-02 10:28:12 +0200

    Add threading extensions.

---
+++ atomtools/thr.py
@@ -0,0 +1,94 @@
+"""Atom Threading Extensions
+
+The document you are looking for is RFC 4685.
+"""
+
+from atomtools.atom import AtomCommon, AtomLink, AtomText
+from atomtools.xml import define_namespace, QName, XMLObject
+
+# Namespaces
+#
+thr_ns = define_namespace("thr", "http://purl.org/syndication/thread/1.0")
+
+class ThrInReplyTo(AtomCommon):
+    """3.  The 'in-reply-to' Extension Element.
+
+    """
+    def __init__(self, ref=None, href=None, source=None, type=None, **kwargs):
+        super(ThrInReplyTo, self).__init__(**kwargs)
+        self.ref = ref
+        self.href = href
+        self.source = source
+        self.type = type
+
+    @classmethod
+    def from_xml(cls, element, **kwargs):
+        return super(ThrInReplyTo, cls).from_xml(element,
+                ref=element.attrib.get("ref"),
+                href=element.attrib.get("href"),
+                source=element.attrib.get("source"),
+                type=element.attrib.get("type"),
+                **kwargs)
+
+    def create_xml(self, parent, tag=QName(thr_ns, "in-reply-to")):
+        return super(ThrInReplyTo, self).create_xml(parent, tag)
+
+    def prepare_xml(self, element):
+        super(ThrInReplyTo, self).prepare_xml(element)
+        if self.ref is not None:
+            element.attrib["ref"] = self.ref
+        if self.href is not None:
+            element.attrib["href"] = self.href
+        if self.source is not None:
+            element.attrib["source"] = self.source
+        if self.type is not None:
+            element.attrib["type"] = self.type
+
+
+class ThrLink(AtomLink):
+    """4.  The 'replies' Link Relation
+
+    """
+    def __init__(self, count=None, updated=None, **kwargs):
+        super(ThrLink, self).__init__(**kwargs)
+        self.count = count
+        self.updated = updated
+
+    @classmethod
+    def from_xml(cls, element, **kwargs):
+        return super(ThrLink, cls).from_xml(element,
+                count=element.attrib.get(QName(thr_ns, "count")),
+                updated=element.attrib.get(QName(thr_ns, "updated")),
+                **kwargs)
+
+    def prepare_xml(self, element):
+        super(ThrLink, self).prepare_xml(element)
+        if self.count is not None:
+            element.attrib[QName(thr_ns, "count")] = self.count
+        if self.updated is not None:
+            element.attrib[QName(thr_ns, "updated")] = self.updated
+
+
+class ThrTotalMixin(XMLObject):
+    """5.  The 'total' Extension Element
+
+    """
+    inner_factor = {
+        "total": AtomText.from_xml
+    }
+
+    def __init__(self, total=None, **kwargs):
+        super(ThrTotalMixin, self).__init__(**kwargs)
+        self.total = total
+
+    @classmethod
+    def from_xml(cls, element, **kwargs):
+        for sub in element:
+            if sub.tag == QName(thr_ns, "total"):
+                kwargs["total"] = cls.inner_from_xml("total", sub)
+        return super(ThrTotalMixin, cls).from_xml(element, **kwargs)
+
+    def prepare_xml(self, element):
+        super(ThrTotalMixin, self).prepare_xml(element)
+        if self.total:
+            self.total.create_xml(element, QName(thr_ns, "total"))
--- atomtools/xml.py
+++ atomtools/xml.py
@@ -4,6 +4,10 @@ from __future__ import absolute_import
 from xml.etree.ElementTree import Element, register_namespace, SubElement
 from xml.etree.ElementTree import parse as xml_parse
 
+# Imports carried over. You can encouraged to import these names from here
+#
+from xml.etree.ElementTree import QName
+
 def define_namespace(prefix, url):
     register_namespace(prefix, url)
     return url
