091078361556d86c74b74465c7ff7c0e13b3389e
Martin Hoffmann <hn@nvnc.de>
2012-07-18 18:15:09 +0200

    Merge ames into asoc.

--- atomtools/asoc.py
+++ atomtools/asoc.py
@@ -7,11 +7,15 @@ document of an account and a view documents accessibly via those links.
 
 This is work in progress and will be extended as necessary. And probably
 changed completely.
+
+You can find the documentation in doc/ames in the source distribution.
 """
 
-from atomtools.atom import AtomCategory, atom_ns
-from atomtools.atompub import AppService
-from atomtools.utils import create_text_xml
+from atomtools.atom import (AtomCategory, AtomCommon, AtomDate,
+                            AtomEntry, AtomLink, AtomPerson, AtomText,
+                            atom_ns)
+from atomtools.atompub import AppFeed, AppService
+from atomtools.utils import create_text_xml, from_text_xml
 from atomtools.xml import define_namespace, QName, XMLObject, xml_ns
 
 # Namespace
@@ -19,36 +23,133 @@ from atomtools.xml import define_namespace, QName, XMLObject, xml_ns
 asoc_ns = define_namespace("asoc", "http://www.alipedis.com/2012/asoc")
 
 
-class AsocLink(XMLObject):
-    """The "asoc:link" Element
+# Messaging
+#
 
-    Links to the various social networking documents.
+class AsocPost(AtomCommon):
+    """An entry without requirement to have a title.
+    
     """
-    standard_tag = QName(asoc_ns, "link")
+    inner_factory = {
+        "author": AtomPerson.from_xml,
+        "category": AtomCategory.from_xml,
+        "content": AtomText.from_xml,
+        "link": AtomLink.from_xml,
+        "published": AtomDate.from_xml,
+        "rights": AtomText.from_xml,
+        "updated": AtomDate.from_xml,
+    }
+    standard_tag = QName(asoc_ns, "post")
+    content_type = "application/asoc+xml"
 
-    def __init__(self, href=None, rel=None, base=None, **kwargs):
-        super(AsocLink, self).__init__(**kwargs)
-        self.href = href
-        self.rel = rel
-        self.base = base
+    def __init__(self, authors=(), categories=(), content=None, id=None,
+                 links=(), published=None, rights=None, updated=None,
+                 **kwargs):
+        super(AsocPost, self).__init__(**kwargs)
+        self.authors = list(authors)
+        self.categories = list(categories)
+        self.content = content
+        self.id = id
+        self.links = list(links)
+        self.published = published
+        self.rights = rights
+        self.updated = updated
 
     @classmethod
     def from_xml(cls, element, **kwargs):
-        return super(AsocLink, cls).from_xml(element,
-                href=element.attrib.get("href"),
-                rel=element.attrib.get("rel"),
-                base=element.attrib.get(QName(xml_ns, "base")),
-                **kwargs)
+        kwargs.setdefault("authors", [])
+        kwargs.setdefault("categories", [])
+        kwargs.setdefault("links", [])
+        for sub in element:
+            if sub.tag == QName(atom_ns, "author"):
+                kwargs["authors"].append(cls.inner_from_xml("author", sub))
+            elif sub.tag == QName(atom_ns, "category"):
+                kwargs["categories"].append(cls.inner_from_xml("category",
+                                                               sub))
+            elif sub.tag == QName(asoc_ns, "content"):
+                kwargs["content"] = cls.inner_from_xml("content", sub)
+            elif sub.tag == QName(atom_ns, "id"):
+                kwargs["id"] = from_text_xml(sub)
+            elif sub.tag == QName(atom_ns, "link"):
+                kwargs["links"].append(cls.inner_from_xml("link", sub))
+            elif sub.tag == QName(atom_ns, "publised"):
+                kwargs["published"] = cls.inner_from_xml("published", sub)
+            elif sub.tag == QName(atom_ns, "rights"):
+                kwargs["rights"] = cls.inner_from_xml("rights", sub)
+            elif sub.tag == QName(atom_ns, "updated"):
+                kwargs["updated"] = cls.inner_from_xml("updated", sub)
+        return super(AsocPost, cls).from_xml(element, **kwargs)
 
     def prepare_xml(self, element):
-        super(AsocLink, self).prepare_xml(element)
-        if self.href is not None:
-            element.attrib["href"] = self.href
-        if self.rel is not None:
-            element.attrib["rel"] = self.rel
-        if self.base is not None:
-            element.attrib[QName(xml_ns, "base")] = self.base
+        super(AsocPost, self).prepare_xml(element)
+        for author in self.authors:
+            author.create_xml(element, QName(atom_ns, "author"))
+        for category in self.categories:
+            category.create_xml(element, QName(atom_ns, "category"))
+        if self.content:
+            self.content.create_xml(element, QName(asoc_ns, "content"))
+        if self.id is not None:
+            create_text_xml(self.id, element, QName(atom_ns, "id"))
+        for link in self.links:
+            link.create_xml(element, QName(atom_ns, "link"))
+        if self.published:
+            self.published.create_xml(element, QName(atom_ns, "published"))
+        if self.rights:
+            self.rights.create_xml(element, QName(atom_ns, "rights"))
+        if self.updated:
+            self.updated.create_xml(element, QName(atom_ns, "updated"))
 
+    # A bunch of helpers to make life easier
+    #
+    def get_link(self, rel):
+        """Return the href of the first link with *rel* or None."""
+        for link in self.links:
+            if link.rel == rel:
+                return link.href
+        return None
+
+    def get_links(self, rel):
+        """Return a list of the hrefs of all links with *rel*."""
+        return [link.href for link in self.links if link.rel == rel]
+
+    def get_first_link(self, rel):
+        """Get the href of the first link with *rel*."""
+        for link in self.links:
+            if link.rel == rel:
+                return link.href
+
+    def replace_link(self, rel, href, **kwargs):
+        """Replace all links with *rel* with a single new one."""
+        self.remove_links(rel)
+        self.links.append(AtomLink(href=href, rel=rel, **kwargs))
+
+    def remove_links(self, rel):
+        """Remove all linjks with *rel*."""
+        self.links = [link for link in self.links if link.rel != rel]
+
+
+class AsocFeed(AppFeed):
+    """An atom:feed with asoc:posts.
+    
+    """
+    inner_factory = {
+        "post": AsocPost.from_xml,
+    }
+
+    def __init__(self, posts=(), **kwargs):
+        super(AsocFeed, self).__init__(**kwargs)
+        self.posts = list(posts)
+
+    @classmethod
+    def from_xml(cls, element, **kwargs):
+        entries = kwargs.setdefault("entries", [])
+        for sub in element:
+            if sub.tag == QName(asoc_ns, "post"):
+                entries.append(cls.inner_from_xml("post", sub))
+        return super(AsocFeed, cls).from_xml(element, **kwargs)
+
+# Peers
+#
 
 class AsocPeer(XMLObject):
     """The "asoc:peer" Element.
@@ -56,10 +157,10 @@ class AsocPeer(XMLObject):
     """
     inner_factory = {
         "category": AtomCategory.from_xml,
-        "link": AsocLink.from_xml,
+        "link": AtomLink.from_xml,
     }
     standard_tag = QName(asoc_ns, "peer")
-    content_type = "application/asoc+xml;type=peer"
+    content_type = "application/asoc+xml"
 
     def __init__(self, uri=None, name=None, categories=(), links=(),
                  **kwargs):
@@ -75,13 +176,13 @@ class AsocPeer(XMLObject):
         kwargs.setdefault("links", [])
         for sub in element:
             if sub.tag == QName(asoc_ns, "uri"):
-                kwargs["uri"] = sub.text
+                kwargs["uri"] = from_text_xml(sub)
             elif sub.tag == QName(asoc_ns, "name"):
-                kwargs["name"] = sub.text
+                kwargs["name"] = from_text_xml(sub)
             elif sub.tag == QName(atom_ns, "category"):
                 kwargs["categories"].append(cls.inner_from_xml("category",
                                                                sub))
-            elif sub.tag == QName(asoc_ns, "link"):
+            elif sub.tag == QName(atom_ns, "link"):
                 kwargs["links"].append(cls.inner_from_xml("link", sub))
         return super(AsocPeer, cls).from_xml(element, **kwargs)
 
@@ -105,7 +206,7 @@ class AsocPeers(XMLObject):
         "peer": AsocPeer.from_xml,
     }
     standard_tag = QName(asoc_ns, "peers")
-    content_type = "application/asoc+xml;type=peers"
+    content_type = "application/asoc+xml"
 
     def __init__(self, peers=(), **kwargs):
         super(AsocPeers, self).__init__(**kwargs)
@@ -130,6 +231,7 @@ class AsocCertificate(XMLObject):
 
     """
     standard_tag = QName(asoc_ns, "certificate")
+    content_type = "application/asoc+xml"
 
     def __init__(self, href=None, name=None, certificate=None, **kwargs):
         super(AsocCertificate, self).__init__(**kwargs)
@@ -164,7 +266,7 @@ class AsocCertificates(XMLObject):
         "certificate": AsocCertificate.from_xml,
     }
     standard_tag = QName(asoc_ns, "certificates")
-    content_type = "application/asoc+xml;type=certificates"
+    content_type = "application/asoc+xml"
 
     def __init__(self, certificates=(), **kwargs):
         super(AsocCertificates, self).__init__(**kwargs)
@@ -190,7 +292,7 @@ class AsocService(AppService):
 
     """
     inner_factory = {
-        'link': AsocLink.from_xml,
+        'link': AtomLink.from_xml,
     }
 
     def __init__(self, links=(), **kwargs):
@@ -201,7 +303,7 @@ class AsocService(AppService):
     def from_xml(cls, element, **kwargs):
         kwargs.setdefault('links', [])
         for sub in element:
-            if sub.tag == QName(asoc_ns, "link"):
+            if sub.tag == QName(atom_ns, "link"):
                 kwargs["links"].append(cls.inner_from_xml("link", sub))
         return super(AsocService, cls).from_xml(element, **kwargs)
 
